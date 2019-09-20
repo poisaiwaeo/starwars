264da618f9252e32eaa7c8fbbe16705f1eea107d
poisaiwaeo <54976146+poisaiwaeo@users.noreply.github.com>
2019-09-20 12:29:01 +0200

    Opgave afsluttet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+  //https://swapi.co/api/people/
+
+  state = {
+    personer: []
+  }
+  //Hent data når component er mount/klar
+  componentDidMount(){
+    
+    //Brug axios til at kontakt + hente data fra API
+    //Tage det her API axios.get ('https://swapi.co/api/people/') og giver resultat
+    axios.get ('https://swapi.co/api/people/').then(resultat => { 
+
+      console.log(resultat.data.results[5].name);
+
+      this.setState({
+        personer:resultat.data.results
+      });
+
+    });
+  }
+
+  render() {
+
+    const allepersoner = this.state.personer;
+
+    //map´er (kortlægger) data - hvordan og hvad der skal vises
+    const personListe = allepersoner.map( (person,index) => {
+
+      return(
+        <div key={index} className="startekst__container">
+           <div className="person__tekst">
+            Navn: {person.name} 
+            <br/>
+            Højde: {person.height} 
+            <br/>
+            Født: {person.birth_year}
+            </div>
+        </div>
+      )
+    });
+
+    return (
+      <div>
+        
+        <h1>Star Wars</h1>
+        <h2>Personer</h2>
+        {personListe}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
